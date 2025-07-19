0f1ab84881452cb521f5a78322aa5431059f1a34
kjyook <106497893+kjyook@users.noreply.github.com>
2025-07-19 20:27:20 +0900

    feat: openapi-generator typescript-fetch generate

---
+++ src/lib/models/index.ts
@@ -0,0 +1,21 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './BadgeRequest';
+export * from './BadgeResponse';
+export * from './CreateRequest';
+export * from './KakaoLoginRequest';
+export * from './LoginRequest';
+export * from './LoginResponse';
+export * from './QuestionSeed';
+export * from './QuizResponse';
+export * from './QuizResultSummary';
+export * from './QuizSubmitRequest';
+export * from './Record';
+export * from './RecordCreateRequest';
+export * from './Response';
+export * from './SkipStatusResponse';
+export * from './StreakCountResponse';
+export * from './UpdateRequest';
+export * from './UserBadgeRequest';
+export * from './UserBadgeResponse';
+export * from './UserInfo';
