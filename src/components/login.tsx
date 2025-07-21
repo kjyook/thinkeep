e7a5895f2e4896229f703f116be76acdaa471a70
lnylnylnylny <lnylnylnylny@naver.com>
2025-07-21 11:03:50 +0900

    docs: 주석 추가

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -3,10 +3,13 @@ import { signIn } from 'next-auth/react';
 
 const LoginBtn = () => {
   return (
-    <button className="bg-amber-500 w-full h-full" onClick={() => signIn('kakao')}>
+    // callbackUrl은 로그인 이후 리다이렉트할 URL을 지정
+    <button
+      className="bg-amber-500 w-full h-full"
+      onClick={() => signIn('kakao', { callbackUrl: '/' })}
+    >
       카카오 로그인
     </button>
   );
 };
 export default LoginBtn;
-// , { callbackUrl: '/' }
